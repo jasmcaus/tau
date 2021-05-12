7831797338ae6c8a3c921f7f971825414f60f567
Jason Dsouza <jasmcaus@gmail.com>
2021-05-12 17:23:20 +0530

    Update Arrow

--- Arrow/UTest.h
+++ Arrow/UTest.h
@@ -337,14 +337,6 @@ static inline Int64 arrow_ns(void) {
     #define ARROW_NULL      0
 #endif
 
-// Coloured output
-#ifdef _MSC_VER
-    #define ARROW_USE_COLOUR_OUTPUT() (_isatty(_fileno(stdout)))
-#else
-    #include <unistd.h>
-    #define ARROW_USE_COLOUR_OUTPUT() (isatty(STDOUT_FILENO))
-#endif
-
 static inline void* arrow_realloc(void* const ptr, Ll new_size) {
   void* const new_ptr = realloc(ptr, new_size);
 
@@ -384,14 +376,85 @@ static char* arrow_argv0_ = null;
     #define ARROW_UNUSED   __attribute__((unused))
 #endif
 
-#ifdef __clang__
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wvariadic-macros"
-    #pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
+// Coloured output
+#ifdef _MSC_VER
+    #define ARROW_USE_COLOUR_OUTPUT() (_isatty(_fileno(stdout)))
+#else
+    #include <unistd.h>
+    #define ARROW_USE_COLOUR_OUTPUT() (isatty(STDOUT_FILENO))
 #endif
 
+// Overridden in `arrow_main` if the cmdline option `--no-color` is passed
+static bool arrow_should_colourize_output = ARROW_USE_COLOUR_OUTPUT();
 
-// ADD COLOURED_PRINTF
+#define ARROW_COLOR_DEFAULT_              0
+#define ARROW_COLOR_GREEN_                1
+#define ARROW_COLOR_RED_                  2
+#define ARROW_COLOR_DEFAULT_INTENSIVE_    3
+#define ARROW_COLOR_GREEN_INTENSIVE_      4
+#define ARROW_COLOR_RED_INTENSIVE_        5
+
+static int ARROW_ATTRIBUTE_(format (printf, 2, 3))
+arrow_colored_printf_(int color, const char* fmt, ...) {
+    va_list args;
+    char buffer[256];
+    int n;
+
+    va_start(args, fmt);
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+    buffer[sizeof(buffer)-1] = '\0';
+
+    if(!arrow_should_colourize_output) {
+        return printf("%s", buffer);
+    }
+
+#if defined ARROW_UNIX_
+    {
+        const char* col_str;
+        switch(color) {
+            case ARROW_COLOR_GREEN_:              col_str = "\033[0;32m"; break;
+            case ARROW_COLOR_RED_:                col_str = "\033[0;31m"; break;
+            case ARROW_COLOR_GREEN_INTENSIVE_:    col_str = "\033[1;32m"; break;
+            case ARROW_COLOR_RED_INTENSIVE_:      col_str = "\033[1;31m"; break;
+            case ARROW_COLOR_DEFAULT_INTENSIVE_:  col_str = "\033[1m"; break;
+            default:                                col_str = "\033[0m"; break;
+        }
+        printf("%s", col_str);
+        n = printf("%s", buffer);
+        printf("\033[0m");
+        return n;
+    }
+#elif defined ARROW_WIN_
+    {
+        HANDLE h;
+        CONSOLE_SCREEN_BUFFER_INFO info;
+        WORD attr;
+
+        h = GetStdHandle(STD_OUTPUT_HANDLE);
+        GetConsoleScreenBufferInfo(h, &info);
+
+        switch(color) {
+            case ARROW_COLOR_GREEN_:              attr = FOREGROUND_GREEN; break;
+            case ARROW_COLOR_RED_:                attr = FOREGROUND_RED; break;
+            case ARROW_COLOR_GREEN_INTENSIVE_:    attr = FOREGROUND_GREEN | FOREGROUND_INTENSITY; break;
+            case ARROW_COLOR_RED_INTENSIVE_:      attr = FOREGROUND_RED | FOREGROUND_INTENSITY; break;
+            case ARROW_COLOR_DEFAULT_INTENSIVE_:  attr = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY; break;
+            default:                                attr = 0; break;
+        }
+        if(attr != 0)
+            SetConsoleTextAttribute(h, attr);
+        n = printf("%s", buffer);
+        SetConsoleTextAttribute(h, info.wAttributes);
+        return n;
+    }
+#else
+    n = printf("%s", buffer);
+    return n;
+#endif
+}
+
+
 
 
 #define ARROW_PRINTF(...)                                                      \
@@ -407,9 +470,6 @@ static char* arrow_argv0_ = null;
     #define ARROW_SNPRINTF(...) snprintf(__VA_ARGS__)
 #endif
 
-#ifdef __clang__
-    #pragma clang diagnostic pop
-#endif
 
 #ifdef __cplusplus
     /* if we are using c++ we can use overloaded methods (its in the language) */
