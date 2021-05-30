9705f63093940dbdcf7f5ab3dcbf00c6e6b49fac
Jason Dsouza <jasmcaus@gmail.com>
2021-05-30 13:19:15 +0530

    Split `test_assertion_macros` into 2 separate source files (MSVC2017 runs out of compiler heap space

--- test/InternalTests/generateDeathTests.py
+++ test/InternalTests/generateDeathTests.py
@@ -8,8 +8,7 @@ import random
 random.seed(3)
 
 INCLUDE_THIS_BEFORE_EACH_FILE = "#include <Muon/Muon.h>\n\n"
-
-TEST_SUITE_BEGIN = "TEST(gen_tests_%(lang)s, STRING_ASSERTIONS_%(macro)s) {\n"
+TEST_SUITE_BEGIN = "TEST(gen_tests_%(lang)s, DEATHTESTS_%(macro)s) {\n"
 TEST_SUITE_END = "}\n\n"
 
 def generate_random_string(n=None):
@@ -22,7 +21,7 @@ def generate_random_string(n=None):
     return ''.join(random.choice(choices) for i in range(n))
 
 def generated_random_string_data():
-    a = open('random_generated_strings.txt', 'w')
+    a = open('DeathTests/random_generated_strings.txt', 'w')
     try:
         for _ in range(2000):
             rand1 = generate_random_string()
@@ -32,7 +31,7 @@ def generated_random_string_data():
         a.close()
 
 # Begin with the String Macros
-def test_string_macros(lang: str, filename: str ='test_string_macros'):
+def test_string_macros(lang: str, filename: str ='DeathTests/test_string_macros'):
     if filename.endswith('.c'):
         filename = filename[:-2]
     if filename.endswith('.cpp'):
@@ -122,15 +121,16 @@ def test_string_macros(lang: str, filename: str ='test_string_macros'):
     finally:
         a.close()
 
-def test_assertion_macros(lang: str, filename: str ='test_assertion_macros'):
-    if filename.endswith('.c'):
-        filename = filename[:-2]
-    if filename.endswith('.cpp'):
-        filename = filename[:-3]
+# Because of the huge size of the resulting `.c` or `.cpp` file (~7k lines) when this script runs (MSVC 2017 runs out of 
+# heap space because of this), we split the files into 2 seperate source files
+def test_assertion_macros(lang: str, filename: str ='DeathTests/test_assertion_macros'):
+    filename1 = filename + '_1'
+    filename2 = filename + '_2'
     
     # Add the language extension
-    filename = filename + '.' + lang
-    print(f'[INFO] Writing {filename}')
+    filename1 = filename1 + '.' + lang
+    filename2 = filename2 + '.' + lang
+    print(f'[INFO] Writing {filename1} and {filename2}')
 
     ASSERTION_MACROS_TEXT = """    %(macro)s(%(num1)s, %(num2)s);\n"""
     ASSERTION_MACROS = [
@@ -139,53 +139,67 @@ def test_assertion_macros(lang: str, filename: str ='test_assertion_macros'):
     ]
 
     # Open a file to write
-    a = open(filename, 'w')
-    intdata = open('random_integers.txt', 'r').readlines()
+    f1 = open(filename1, 'w')
+    f2 = open(filename2, 'w')
+
+    # Initially, set f to f1
+    f = f1 
+
+    intdata = open('DeathTests/random_integers.txt', 'r').readlines()
+    count = 0
 
     try:
-        a.write(INCLUDE_THIS_BEFORE_EACH_FILE)
-        a.write(TEST_SUITE_BEGIN % {'lang': lang, 'macro' : 'ASSERTION_MACROS'})
+        f1.write(INCLUDE_THIS_BEFORE_EACH_FILE)
+        f1.write(TEST_SUITE_BEGIN % {'lang': lang, 'macro' : 'ASSERTION_MACROS_1'})
+        f2.write(INCLUDE_THIS_BEFORE_EACH_FILE)
+        f2.write(TEST_SUITE_BEGIN % {'lang': lang, 'macro' : 'ASSERTION_MACROS_2'})
 
         for line in intdata:
             line = line.strip().strip('\n').split(',')
             line[1] = line[1].strip()
+            count += 1
+            # Half of the 1500 lines in `random_integers.txt`
+            if count == 750:
+                f = f2
 
             num1 = int(line[0])
             num2 = int(line[1])
 
             if num1 < num2:
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_LT','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_LT','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_GT','num1': str(num2),'num2': str(num1)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_GT','num1': str(num2),'num2': str(num1)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_LE','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_LE','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_LT','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_LT','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_GT','num1': str(num2),'num2': str(num1)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_GT','num1': str(num2),'num2': str(num1)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_LE','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_LE','num1': str(num1),'num2': str(num2)})
 
             elif num1 > num2:
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_GT','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_GT','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_LT','num1': str(num2),'num2': str(num1)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_LT','num1': str(num2),'num2': str(num1)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_GE','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_GE','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_GT','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_GT','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_LT','num1': str(num2),'num2': str(num1)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_LT','num1': str(num2),'num2': str(num1)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_GE','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_GE','num1': str(num1),'num2': str(num2)})
 
             elif num1 == num2:
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_EQ','num1': str(num1),'num2': str(num2),})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_EQ','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_GE','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_GE','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_LE','num1': str(num1),'num2': str(num2)})
-                a.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_LE','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_EQ','num1': str(num1),'num2': str(num2),})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_EQ','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_GE','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_GE','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'CHECK_LE','num1': str(num1),'num2': str(num2)})
+                f.write(ASSERTION_MACROS_TEXT % {'macro': 'REQUIRE_LE','num1': str(num1),'num2': str(num2)})
 
-        a.write(TEST_SUITE_END)
+        f1.write(TEST_SUITE_END)
+        f2.write(TEST_SUITE_END)
     finally:
-        a.close()
+        f1.close()
+        f2.close()
     
 
 if __name__ == '__main__':
     languages = ['c', 'cpp']
-    generated_random_string_data()
+    # generated_random_string_data()
 
     for i in languages:
-        # test_string_macros(str(i))
+        test_string_macros(str(i))
         test_assertion_macros(str(i))
